319f3ac9a25523da9258b068d15480ca018cb791
Cara Comfort <cara.comfort10@gmail.com>
2017-07-05 15:36:11 -0700

    Initial Commit

---
+++ fitness tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fitness tracker
+//
+//  Created by Cara E Comfort on 7/5/17.
+//  Copyright © 2017 Cara E Comfort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
