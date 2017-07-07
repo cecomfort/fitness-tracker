4c31da2ff7af88246708574db8ceeaeecc0d0284
Cara Comfort <cara.comfort10@gmail.com>
2017-07-07 10:23:34 -0700

    Place a pin on a map at the users current location

--- fitness tracker/ViewController.swift
+++ fitness tracker/ViewController.swift
@@ -7,18 +7,75 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+
+    var coordinates: [CLLocationCoordinate2D] = []
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    @IBOutlet weak var currentLatitude: UILabel!
+    @IBOutlet weak var currentLogitude: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // generates authorization popup
+        locationManager.requestWhenInUseAuthorization()
+        
+        // location updates may be pasued if the user does not move a significant distance over a period of time
+        locationManager.activityType = .fitness
+        // looks for GPS coordinates of iphone. This method is asynchronous!
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        
+        // check to see if location is valid
+        if location.horizontalAccuracy > 0 {
+            // stop requesting user location
+            locationManager.stopUpdatingLocation()
+            
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            
+            // Change coordinates on screen
+            currentLatitude.text = "Latitdue = " + String(format: "%.4f", latitude)
+            currentLogitude.text = "Longitude = " + String(format: "%.4f", longitude)
+            
+            // Zooming map to user location
+            let span : MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+            let userLocation : CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+            let region : MKCoordinateRegion = MKCoordinateRegionMake(userLocation, span)
+            map.setRegion(region, animated: true)
+            
+            // Create coordinate
+            let startAnnotation = MKPointAnnotation()
+            startAnnotation.title = "Start"
+            startAnnotation.coordinate = userLocation
+            self.map.addAnnotation(startAnnotation)
+            //        self.map.userTrackingMode
+//            self.map.showsUserLocation = true
+        }
+        
+    }
+
 
 
 }
